68dcfb930bdf125609306b3ae099b68399f5bdac
chateauxvt <tolgaoa@vt.edu>
2022-08-30 18:56:36 -0400

    cleanup

--- run_all.sh
+++ run_all.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#/bin/bash ./charts/slice_confxl_nodeplog.sh 51 3 1 n4kul 4kaggrdu
+/bin/bash ./charts/slice_confxl_nodeplog.sh 3 1 1 n4kul 4kaggrdu
 #/bin/bash ./charts/slice_confxl_nodeplog.sh 51 3 1 ngamingdua gaming
 #/bin/bash ./charts/slice_confxl_nodeplog.sh 51 3 1 nzoomdua hbo
 #/bin/bash ./charts/slice_confxl_nodeplog.sh 51 3 1 nhbodua zoom
-/bin/bash ./charts/slice_confxl_nodeplog.sh 51 3 1 nmiotdua miot
+#/bin/bash ./charts/slice_confxl_nodeplog.sh 2 3 1 nmiotdua miot
 
