91b638fc4abfd851199fe1d892fc60075144af28
chateauxvt <tolgaoa@vt.edu>
2023-05-30 18:25:26 -0400

    removed the monitoring proxies and converted to http2 for mantisnet integrations

--- run.sh
+++ run.sh
@@ -16,17 +16,17 @@ NORMAL=$(tput sgr0)
 #gnbsimim dnnim users slices iterationstest thrtesttype(1==hostlvl, 0==podlvl)
 st=`date +%s`
 
-/bin/bash ./deploy.sh zoomv3 zoomv3 10 8 20 1
+/bin/bash ./deploy.sh zoomv3 zoomv3 1 1 1 1
 
 et=`date +%s`
 rt=$((et-st))
 echo $rt >> ite1.txt
 
-/bin/bash ./deploy.sh netflixv3 netflixv3 10 8 20 1
-/bin/bash ./deploy.sh oculusv3 oculusv3 10 8 20 1
-/bin/bash ./deploy.sh tiktokv3 tiktokv3 10 8 20 1
-/bin/bash ./deploy.sh whatsappv3 whatsappv3 10 8 20 1
-/bin/bash ./deploy.sh fortnitev3 fortnitev3 10 8 20 1
+#/bin/bash ./deploy.sh netflixv3 netflixv3 10 8 20 1
+#/bin/bash ./deploy.sh oculusv3 oculusv3 10 8 20 1
+#/bin/bash ./deploy.sh tiktokv3 tiktokv3 10 8 20 1
+#/bin/bash ./deploy.sh whatsappv3 whatsappv3 10 8 20 1
+#/bin/bash ./deploy.sh fortnitev3 fortnitev3 10 8 20 1
 
 et=`date +%s`
 rt=$((et-st))
